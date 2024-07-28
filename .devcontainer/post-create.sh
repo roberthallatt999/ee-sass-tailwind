fdce32bb82c269ad4b24ad756b37f1549f6dc2ef
Robert Hallatt <roberthall0211@gmail.com>
2024-07-28 05:22:40 +0000

    Modify docker-compose PHP image to use version saved in container registry and remove build step in post-create.sh. No need ot keep running it.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,9 +2,14 @@
 
 set -eux
 
-## Build PHP Dockerfile image
+# Build PHP Dockerfile image - Last image built on Sat Jul 27.
+# First command is how you build an image in order to save it to Github 
+# container registry. Second command is how you push it. 
 
-docker build -t php:latest .
+# docker build . --tag ghcr.io/roberthallatt999/php81:latest
+# add this to your bsh or zshrc file - export CR_PAT={your-github-classic-token}
+# echo $CR_PAT | docker login ghcr.io -u roberthallatt999 --password-stdin
+# docker push ghcr.io/roberthallatt999/php81:latest
 
 ## Install build tool
 
